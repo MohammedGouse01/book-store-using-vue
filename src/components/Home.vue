934c69cfee36a28ffeac758840ff1eb7614489af
MohammedGouse01 <gousetaj001@gmail.com>
2023-08-16 06:20:18 +0530

    home and signup page

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+<img class="logo" src="./../assets/bookLogo.png" alt="logo">
+    <h1 margin-block-start = "0px" margin-block-end= "0px">Hi user!, Welcome to Book Store</h1>
+</template>
+<script>
+
+
+export default {
+    name:'Home',
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+<img class="logo" src="./../assets/bookLogo.png" alt="logo">
+    <h1>Hi user!, Sign Up or Login to read books.</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter Name">
+        <input type="text" placeholder="Enter Email">
+        <input type="password" placeholder="Enter Password">
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:'SignUp',
+}
+</script>
+<style>
+
+</style>
